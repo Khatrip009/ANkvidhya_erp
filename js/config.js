02031a8d51ae8df9857f18c58e14690cb025c353
Khatrip009 <Khatrip.009@gmail.com>
2025-10-03 21:11:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // public/js/config.js
 window.CONFIG = {
-  API_BASE: 'https://hallucinational-ratlike-coralie.ngrok-free.dev', // <- set your ngrok url
+  API_BASE: 'https://hallucinational-ratlike-coralie.ngrok-free.dev',
 };
